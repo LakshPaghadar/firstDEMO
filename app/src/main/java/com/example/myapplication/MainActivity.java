7811c0d62a5fb8fe64b90cc5d77fcbcd9d8ff48a
laksh patel <lakshy1662001@gmail.com>
2021-11-01 11:17:25 +0530

    addition

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Firstnum;
+    private EditText secondnum;
+    private EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firstnum = findViewById(R.id.editTextTextPersonName);
+
+        secondnum = findViewById(R.id.editTextTextPersonName2);
+        int fog =Integer.parseInt( Firstnum.getText().toString() + secondnum.getText().toString());
+        result.setText("result: " + fog);
     }
 }
